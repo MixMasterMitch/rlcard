a2ce3b15939e2107e99a5ea349691c76b53e661f
Mitchell Loeppky <mitchell@loeppky.com>
2022-03-14 10:46:07 -0700

    Disable GPU

--- rlcard/utils/utils.py
+++ rlcard/utils/utils.py
@@ -19,12 +19,15 @@ def set_seed(seed):
 
 def get_device():
     import torch
-    if torch.cuda.is_available():
-        device = torch.device("cuda:0")
-        print("--> Running on the GPU")
-    else:
-        device = torch.device("cpu")
-        print("--> Running on the CPU")
+    # if torch.cuda.is_available():
+    #     device = torch.device("cuda:0")
+    #     print("--> Running on the GPU")
+    # else:
+    #     device = torch.device("cpu")
+    #     print("--> Running on the CPU")
+
+    device = torch.device("cpu")
+    print("--> Running on the CPU")
 
     return device
 
